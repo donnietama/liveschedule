3b91614e0e060f884370440bbd7c4e8c43cd8804
Donny Pratama <donnietama8@gmail.com>
2018-06-26 09:59:29 +0700

    added new folder for json file and added new path to fetch from webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/json/data.json', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
