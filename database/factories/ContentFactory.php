376451e075d0ccb61a18349cd9ea1999c107291c
Donny Pratama <donnietama8@gmail.com>
2018-07-17 12:00:13 +0700

    changed date format

--- database/factories/ContentFactory.php
+++ database/factories/ContentFactory.php
@@ -4,18 +4,18 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Content::class, function (Faker $faker) {
     return [
-        'month' => $faker->date($format = 'Y-M-d', $max = 'now'),
+        'month' => $faker->date($format = 'F', $max = 'now'),
         'opening_estimation' => $faker->date($format = 'Y-M-d'),
         'store_location' => $faker->address,
         'store_owner' => $faker->name,
-        'h_44' => $faker->date($format = 'Y-M'),
-        'h_32' => $faker->date($format = 'Y-M'),
-        'h_22' => $faker->date($format = 'Y-M'),
-        'h_21' => $faker->date($format = 'Y-M'),
-        'h_8' => $faker->date($format = 'Y-M'),
-        'h_3' => $faker->date($format = 'Y-M'),
-        'h_2' => $faker->date($format = 'Y-M'),
-        'h' => $faker->date($format = 'Y-M'),
+        'h_44' => $faker->date($format = 'd-M'),
+        'h_32' => $faker->date($format = 'd-M'),
+        'h_22' => $faker->date($format = 'd-M'),
+        'h_21' => $faker->date($format = 'd-M'),
+        'h_8' => $faker->date($format = 'd-M'),
+        'h_3' => $faker->date($format = 'd-M'),
+        'h_2' => $faker->date($format = 'd-M'),
+        'h' => $faker->date($format = 'd-M'),
         'additional_info' => $faker->sentence,
         'finished' => $faker->boolean,
         'accountability' => $faker->name
