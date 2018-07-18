6678275d4a0c5b1fdc2149155b05d6211b7eb7d4
Donny Pratama <donnietama8@gmail.com>
2018-07-19 04:23:48 +0700

    added previous link and font-awesome

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-2 bg-dark p-0">
     <ul class="text-capitalize p-0" style="list-style: none; height: 100vh;">
-        <a href="#">
+        <a href="{{ url()->previous() }}">
             <li class="bg-success text-white p-4">kembali ke halaman awal</li>
         </a>
         <a href="/create">
