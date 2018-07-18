7617a52325c236e3eb9dd17304d340c05c44cabd
Donny Pratama <donnietama8@gmail.com>
2018-07-19 03:54:12 +0700

    separating sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid p-0">
-    <div class="row">
+<div class="container-fluid">
+    <div class="row p-0">
         <div class="col-md-2 bg-dark p-0">
             <ul class="text-capitalize p-0" style="list-style: none; height: 100vh;">
                 <a href="#">
